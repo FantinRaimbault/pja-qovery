2fd11e6b97fdaed0a6bda472167240a10e8162e5
fantin <fantin@malou.io>
2021-07-16 15:49:48 +0200

    add

---
+++ src/Modules/Middlewares/IsEntityFound.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Modules\Middlewares;
+
+use App\Core\Logger;
+use App\Core\Router\Middleware;
+
+class IsEntityFound extends Middleware
+{
+    public function __construct(array $entities)
+    {
+        $this->entities = $entities;
+    }
+
+    public function handle($params)
+    {
+        foreach ($this->entities as $entityName => $entityIdFormat) {
+            $id = $params[$entityIdFormat];
+            $entityName = $entityName;
+            $classe = "\App\Modules\Controllers\\$entityName" . "s\Models\\$entityName";
+            $entity = (new $classe(['id' => $id]))->getById();
+            if(empty($entity)) {
+                throw new \Exception("Entity $entityName Not found", 404);
+            }
+        }
+    }
+}
